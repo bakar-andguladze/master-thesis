add57cfc4b9532ea6821fc2a54bdd1518cd7ccdf
ge34jit <ge34jit@mytum.de>
2021-11-13 15:40:26 +0100

    test sequence

---
+++ App/sequence_run.sh
@@ -0,0 +1,20 @@
+jq . config1.json
+python run_test.py configs/config1.json
+jq . config2.json
+python run_test.py configs/config2.json
+jq . config3.json
+python run_test.py configs/config3.json
+jq . config4.json
+python run_test.py configs/config4.json
+jq . config5.json
+python run_test.py configs/config5.json
+jq . config6.json
+# python run_test.py configs/config6.json
+# jq . config7.json
+# python run_test.py configs/config7.json
+# jq . config8.json
+# python run_test.py configs/config8.json
+# jq . config9.json
+# python run_test.py configs/config9.json
+# jq . config10.json
+# python run_test.py configs/config10.json
